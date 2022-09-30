15b6127ecfb5888bb9f39e62d5f3f05b3b409b25
Adam Tassara Powell <apowell@umass.edu>
2022-09-30 17:44:37 -0400

    Jam start commit!

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local terebi = require "lib.terebi" -- Screen scaling
 local baton = require "lib.baton" -- Input
-local bitset = require "lib.bitser" -- Serialization
+local bitser = require "lib.bitser" -- Serialization
 
 ------ INITIALIZATION ------
 
@@ -9,6 +9,21 @@ function love.load()
 
     screen = terebi.newScreen(320, 240, 2)
         :setBackgroundColor(0.25, 0.25, 0.25)
+
+    input = baton.new {
+        controls = {
+            left = {'key:left', 'key:a', 'axis:leftx-', 'button:dpleft'},
+            right = {'key:right', 'key:d', 'axis:leftx+', 'button:dpright'},
+            up = {'key:up', 'key:w', 'axis:lefty-', 'button:dpup'},
+            down = {'key:down', 'key:s', 'axis:lefty+', 'button:dpdown'},
+            action_z = {'key:z', 'button:a'},
+            action_x = {'key:x', 'button:b'},
+        },
+        pairs = {
+            move = {'left', 'right', 'up', 'down'}
+        },
+        joystick = love.joystick.getJoysticks()[1],
+    }
 end
 
 ------ UPDATES ------
